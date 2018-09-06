2f866ae2bfc2c972b692049bdc4dc03b3cf44b98
Holben888 <holben888@gmail.com>
2018-09-06 14:35:27 -0400

    finish printOutMessage in Person2.java

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,47 +1,49 @@
 package edu.gatech.oad.antlab.person;
 
 /**
- *  A simple class for person 2
- *  returns their name and a
- *  modified string 
+ * A simple class for person 2 returns their name and a modified string
  *
- * @author Bob
+ * @author Benjamin
  * @version 1.1
  */
 public class Person2 {
-    /** Holds the persons real name */
-    private String name;
-	 	/**
-	 * The constructor, takes in the persons
-	 * name
+	/** Holds the persons real name */
+	private String name;
+
+	/**
+	 * The constructor, takes in the persons name
+	 * 
 	 * @param pname the person's real name
 	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
+	public Person2(String pname) {
+		name = pname;
+	}
+
 	/**
-	 * This method should take the string
-	 * input and return its characters in
-	 * random order.
-	 * given "gtg123b" it should return
-	 * something like "g3tb1g2".
+	 * This method should take the string input and return its characters in random
+	 * order. given "gtg123b" it should return something like "g3tb1g2".
 	 *
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+		String randomized = "";
+		StringBuilder str = new StringBuilder(input);
+		while (str.length() > 0) {
+			int randIndex = (int) (Math.random() * input.length());
+			randomized += str.charAt(randIndex);
+			str.deleteCharAt(randIndex);
+		}
+		return randomized;
 	}
+
 	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
+	 * Return a string rep of this object that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
+	 * @return the string representing the object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
 }
